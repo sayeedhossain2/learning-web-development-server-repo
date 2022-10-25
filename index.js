8cd26db619ce85ed8b5d436d64dfe816ac0fbd9f
sayeedhossain2 <sayeedh95@gmail.com>
2022-10-25 11:32:20 +0600

    categories json added

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const categories = require("./data/categories.json");
 
 app.get("/", (req, res) => {
   res.send("news api running");
+});
+
+app.get("/courses-categories", (req, res) => {
+  res.send(categories);
 });
 
 app.listen(port, () => {
