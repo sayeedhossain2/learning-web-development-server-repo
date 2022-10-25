775fcf37c28500bda491c7e0d03488ccad944757
sayeedhossain2 <sayeedh95@gmail.com>
2022-10-25 17:27:19 +0600

    book  json file added

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require("./data/categories.json");
+const book = require("./data/book.json");
 
 app.get("/", (req, res) => {
   res.send("news api running");
@@ -13,6 +14,22 @@ app.get("/", (req, res) => {
 
 app.get("/courses-categories", (req, res) => {
   res.send(categories);
+});
+
+app.get("/categories/:id", (req, res) => {
+  const id = req.params.id;
+  const category = categories.find((p) => p.id === id);
+  res.send(category);
+});
+
+app.get("/book", (req, res) => {
+  res.send(book);
+});
+
+app.get("/book/:id", (req, res) => {
+  const id = req.params.id;
+  const selectbook = book.filter((p) => p.id === id);
+  res.send(selectbook);
 });
 
 app.listen(port, () => {
